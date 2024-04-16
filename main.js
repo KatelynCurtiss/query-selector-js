3fc7ad09160500d3de22e143e303e7e1be34d2f0
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-04-16 11:12:15 -0400

    updated html, and JS

--- main.js
+++ main.js
@@ -2,4 +2,24 @@
 //16 APR 2024
 //Targeting Page Elements
 
-const student 
+const student = document.querySelector('#student');
+const homeSchool = document.querySelector('#homeschooled');
+const city = document.querySelector('#city');
+
+//Js styling 
+
+document.body.style.backgroundColor = "lightgreen";
+
+student.style.color = 'var(--steelblue)';
+student.style.fontSize = '1.7rem';
+
+homeSchool.style.fontSize = '1.2rem';
+homeSchool.style.marginBottom = '20px';
+homeSchool.style.fontFamily = 'Helvetica, sans-serif';
+homeSchool.style.fontVariant = 'small-caps';
+homeSchool.style.color = 'orange';
+
+city.style.display = 'inline';
+city.style.color = 'salmon';
+city.style.padding = '10px';
+city.style.border = '4px dotted orange';
